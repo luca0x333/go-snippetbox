3ccaa14a4fa333d3c2e2dc0883f9879c83575d53
Luca <luca.nunzi@colossusbets.com>
2021-05-07 16:14:14 +0100

    add middleware secureHeaders and logRequest

---
+++ cmd/web/middleware.go
@@ -0,0 +1,24 @@
+package main
+
+import "net/http"
+
+// secureHeaders add two Http headers to every response.
+// It acts on every request that is received and it will be executed before the request hits servemux.
+// secureHeaders <-> servemux <-> application handler
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Frame-Options", "deny")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// logRequest <-> secureHeaders <-> servemux <-> application handler
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
